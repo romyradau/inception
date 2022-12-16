ec1acf0ebe51c1879a670fc6b9ac8db995216fb3
rschleic <rschleic@HurtigRuten>
2022-12-16 20:21:19 +0100

    git problems

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,30 +2,36 @@
 
 chown -R mysql:mysql /var/lib/mysql
 
+# /etc/init.d/mysql start
 service mysql start
-echo "hallo" <<EOF
-"A"
-"B"
-"C"
-EOF
 
 
-mysql -u root <<EOF
-"USE mysql"
-"FLUSH PRIVILEGES"
-"SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${DB_ROOT_PASSWORD}')"
-"CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
-"CREATE USER IF NOT EXISTS '${DB_USER}'@'' IDENTIFIED BY '${DB_USER_PASSWORD}'"
-"GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'' IDENTIFIED BY '${DB_USER_PASSWORD}' WITH GRANT OPTION"
-"GRANT ALL PRIVILEGES ON *.* TO '${DB_ROOT}'@'%' IDENTIFIED BY '${DB_ROOT_PASSWORD}'"
-"UPDATE mysql.user SET Password=PASSWORD('${DB_ROOT_PASSWORD}') WHERE User='${DB_ROOT}'"
-"UPDATE mysql.user SET plugin = '' WHERE User='${DB_ROOT}' AND host='localhost'"
-"FLUSH PRIVILEGES"
+# mysql -u root <<EOF
+# CREATE DATABASE IF NOT EXISTS $DB_NAME;
+# CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';
+# GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD' WITH GRANT OPTION;
+# GRANT ALL PRIVILEGES ON *.* TO '$DB_ROOT'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';
+# FLUSH PRIVILEGES;
+# UPDATE mysql.user SET Password=PASSWORD('$DB_ROOT_PASSWORD') WHERE User='$DB_ROOT';
+# UPDATE mysql.user SET plugin = '' WHERE User = '$DB_ROOT' AND host = 'localhost';
+# EOF
+mysql --user=root << EOF
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER IF NOT EXISTS 'rschleic'@'%' IDENTIFIED BY 'rschleic';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'rschleic'@'%' IDENTIFIED BY 'rschleic' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'rschleic';
+FLUSH PRIVILEGES;
+
+UPDATE mysql.user SET Password=PASSWORD('rschleic') WHERE User='root';
+UPDATE mysql.user SET plugin = '' WHERE User = 'root' AND host = 'localhost';
 EOF
 # #is the order important/fine?
 # #wofuer sind die Update Zeilen
-#sleep 5
-service mysql stop
+sleep 5
+#i suppose for it to get created
+# mysqladmin -prschleic shutdown
+#for mariadb to not get restarted
+# service mysql stop
 #should I include this?
 
 exec mysqld_safe
