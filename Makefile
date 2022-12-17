487792c7ea57b3eea281768e201e4a9b7e9d32a2
rschleic <rschleic@HurtigRuten>
2022-12-17 18:43:40 +0100

    mariadb works with hardcore delete

---
+++ Makefile
@@ -0,0 +1,23 @@
+.PHONY: all clean fclean debug re
+all:
+	mkdir -p /home/rschleic/data/mariadb
+	mkdir -p /home/rschleic/data/wordpress
+	docker compose  -f ./srcs/docker-compose.yml up -d
+#       -p, --parents
+            #   no error if existing, make parent directories as needed
+clean:
+	docker compose -f ./srcs/docker-compose.yml down 
+
+fclean:
+	docker compose -f ./srcs/docker-compose.yml down --volumes --rmi all
+	sudo rm -rf /home/rschleic/data/mariadb
+	sudo rm -rf /home/rschleic/data/wordpress
+	
+debug:
+	docker system prune -a -f
+
+re: fclean all
+
+light: clean all
+
+hardcore: fclean debug all
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -6,32 +6,24 @@ chown -R mysql:mysql /var/lib/mysql
 service mysql start
 
 
-# mysql -u root <<EOF
-# CREATE DATABASE IF NOT EXISTS $DB_NAME;
-# CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';
-# GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD' WITH GRANT OPTION;
-# GRANT ALL PRIVILEGES ON *.* TO '$DB_ROOT'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';
-# FLUSH PRIVILEGES;
-# UPDATE mysql.user SET Password=PASSWORD('$DB_ROOT_PASSWORD') WHERE User='$DB_ROOT';
-# UPDATE mysql.user SET plugin = '' WHERE User = '$DB_ROOT' AND host = 'localhost';
-# EOF
-mysql --user=root << EOF
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'rschleic'@'%' IDENTIFIED BY 'rschleic';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'rschleic'@'%' IDENTIFIED BY 'rschleic' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'rschleic';
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-
-UPDATE mysql.user SET Password=PASSWORD('rschleic') WHERE User='root';
-UPDATE mysql.user SET plugin = '' WHERE User = 'root' AND host = 'localhost';
+UPDATE mysql.user SET Password=PASSWORD('$DB_ROOT_PASSWORD') WHERE User='$DB_ROOT';
+UPDATE mysql.user SET plugin = '' WHERE User = '$DB_ROOT' AND host = 'localhost';
 EOF
-# #is the order important/fine?
+#TODO:fragwuerdige Zeile *.* ...???
+
+# GRANT ALL PRIVILEGES ON *.* TO '$DB_ROOT'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';
+#TODO:priviliges fuer root @ localhost, weil ja niemadn von aussen auf root zugreifen soll.
 # #wofuer sind die Update Zeilen
 sleep 5
 #i suppose for it to get created
 # mysqladmin -prschleic shutdown
 #for mariadb to not get restarted
-# service mysql stop
+service mysql stop
 #should I include this?
 
 exec mysqld_safe
